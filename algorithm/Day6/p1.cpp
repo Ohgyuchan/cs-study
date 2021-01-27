b75b2071941d0b08e24306d857d12f0770e4c786
Ohgyuchan <gyu3637@gmail.com>
2021-01-27 09:53:08 +0900

    add Day6

--- algorithm/Day6/p1.cpp
+++ algorithm/Day6/p1.cpp
@@ -1,17 +1,31 @@
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+/*
+1. 3장씩 더한다
+2. m과 차이가 가장 적은 것을 구한다.
+*/
 
 int main() {
-  long long a, b, c;
-  long long count = 0;
+  int n, m;
+  int sum = 0, result = 0;
+  cin >> n >> m;
+  int *card = new int[n];
+
+  for(int i = 0; i < n; i++)
+    cin >> card[i];
   
-  scanf("%lld %lld %lld", &a, &b, &c);
-
-  if( b >= c ) {
-      printf("-1");
-      return 0;
+  for(int i = 0; i < n - 2; i++) {
+    for(int j = i + 1; j < n - 1; j++) {
+      for(int k = j + 1; k < n; k++) {
+        sum = card[i] + card[j] + card[k];
+        if(m - sum < m - result && sum <= m )
+          result = sum;
+      }
+    }
   }
-  count = a / (c - b) + 1;
 
-  printf("%lld\n", count);
+  cout << result << endl;
+  delete[] card;
   return 0;
 }

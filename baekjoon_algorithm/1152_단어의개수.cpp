59aff8f7f3abad8aa068fb531a6b4d3551ceb731
Terman <gyu3637@gmail.com>
2021-02-25 19:19:06 +0900

    Create 1152_단어의개수.cpp

---
+++ baekjoon_algorithm/1152_단어의개수.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h> 
+
+int main(void) { 
+    int i, len, cnt = 0; 
+    char arr[1000000]; 
+    scanf("%[^\n]", arr); 
+    len = strlen(arr); 
+    /* 공백 문자 하나만 입력 시 0으로 출력 후 종료 */ 
+    if(len == 1) { 
+        if(arr[i] == ' ') { 
+            printf("0\n"); return 0; 
+        } 
+    } 
+    /* 공백 문자 카운트 */ 
+    for(i = 1; i < len-1; i++) { 
+        if(arr[i] == ' ') cnt++; 
+    } 
+    printf("%d\n", cnt + 1); 
+    return 0; 
+}

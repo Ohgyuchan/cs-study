55a16e0887382c3f470c8f3383d09ef6da7157cf
Ohgyuchan <gyu3637@gmail.com>
2021-06-05 00:28:51 +0900

    Solve 백준 2839 설탕배달

--- baekjoon/8step_basic_math/sugar.c
+++ baekjoon/8step_basic_math/sugar.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 
 int sugar(int n) {
-  int result;
-  if(n < 5 && n % 3)
-    return -1;
-  else if(n % 3 == 0)
-    return n / 3;
-  else if(n % 5 == 0)
-    return n / 5;
-
-  result = n / 5;
-  n = n - (result * 5);
+  int result = 0;
   
-  if(result % 3 != 0)
-    return -1;
-  
-  result += n / 3;
-  
+  while(n > 0) {
+    if(n % 5 == 0) {
+      n -= 5;
+      result++;
+    }
+    else if(n % 3 == 0) {
+      n -= 3;
+      result++;
+    }
+    else if(n > 5) {
+      n -= 5;
+      result++;
+    }
+    else {
+      return -1;
+    }
+  }
   return result;
 }
+
 
 int main() {
   

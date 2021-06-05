e8db6c83613b6246791d40cf227f4c8bca9e7131
Ohgyuchan <gyu3637@gmail.com>
2021-06-05 19:06:36 +0900

    Solve 백준 1011 Fly me to the Alpha Centauri

--- baekjoon/8step_basic_math/1011.c
+++ baekjoon/8step_basic_math/1011.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
   int T;
-  
+  long sr, x, y;
+
   scanf("%d", &T);
-  
-  int x, y;
-  
-  for(int i = 0 ; i < T ; i++) {
-    scanf("%d %d", &x, &y);
-    alpha_centauri(x, y);
+
+  for(int i = 0 ; i < T; i++) {
+    scanf("%ld %ld", &x, &y);
+
+    sr = (int)sqrt(y-x);
+
+    if((y-x) == sr*sr)
+      printf("%ld\n", 2 * sr-1);
+    else if(sr*sr<(y-x) && (y-x) <= sr*sr+sr)
+      printf("%ld\n", 2*sr);
+    else if(sr*sr+sr < (y-x) && (y-x) < (sr+1) * (sr+1))
+      printf("%ld\n", 2*sr+1);
   }
-  
+
+  return 0;
 }

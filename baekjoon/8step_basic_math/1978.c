e77d58c4471b548c598ef4c1a9376380be372e13
Ohgyuchan <gyu3637@gmail.com>
2021-06-06 20:48:56 +0900

    Solve 백준 1978 소수찾기

--- baekjoon/8step_basic_math/1978.c
+++ baekjoon/8step_basic_math/1978.c
@@ -5,26 +5,33 @@
  * 소수(Prime Number): 1과 자기 자신으로만 나누어 떨어지는 수.
  * Solution: 자신의 2부터 자신의 제곱근까지 나눠보면 된다.
  */
+int is_prime_number(int num) {
+  if(num <= 1)
+    return 0;
+  
+  else if(num % 2 == 0) {
+    if(num == 2)
+      return 1;
+    return 0;
+  }
+  
+  for(int i = 3; i <= sqrt(num); i++) {
+    if(num % i == 0)
+      return 0;
+  }
+  return 1;
+}
 int main() {
   int N, pnum;
-  
+  int count = 0;
+
   scanf("%d", &N);
-
-  int count = N;
   
-  for(int i; i < N; i++) {
+  for(int i = 0 ; i < N; i++) {
     scanf("%d", &pnum);
-    if(pnum == 1) {
-      count--;
-      continue;
-    }
-
-    int sq_root = sqrt(pnum) + 1;
-    for(int i = 2; i < sq_root; i++) {
-      if(pnum % i == 0)
-        count--;
-    }
+    count += is_prime_number(pnum);
   }
+  
   printf("%d\n", count);
   return 0;
 }

bf50422ba551f8358543e515f54f7402fd570aa3
Ohgyuchan <gyu3637@gmail.com>
2021-06-18 16:27:12 +0900

    Solve 백준 1929 using 에라토스테네스 체

--- baekjoon/8step_basic_math/1929.c
+++ baekjoon/8step_basic_math/1929.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 
+#define SIZE 1000001
+
+int num[SIZE] = { 0, };
+
 int main() {
   int M, N;
   scanf("%d %d", &M, &N);
 
-  for(int i = M; i <= sqrt(N); i++) {
-    while(N % i == 0) {
+  for(int i = 2; i <= N; i++) {
+    if(!num[i])
+      for(int j = i + i; j <= N; j += i)
+        num[j] = 1;
+    if(i >= M && num[i] == 0)
       printf("%d\n", i);
-      N /= i;
-    }
   }
-
-  if(N != 0)
-    printf("%d\n", N);
-
+    
   return 0;
 }

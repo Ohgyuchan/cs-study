5e6cd06427af815a5a34c68385753109a605c71b
Ohgyuchan <gyu3637@gmail.com>
2021-06-20 20:42:34 +0900

    Solve 백준 9020

--- baekjoon/8step_basic_math/9020.c
+++ baekjoon/8step_basic_math/9020.c
@@ -6,16 +6,21 @@ int num[SIZE] = { 0, };
 
 int main() {
   int T, N;
-
+/*
   for(int i = 2; i <= sqrt(SIZE); i++)
-    for(int j = i * i; j <= SIZE; j+= i)
+    for(int j = i * i; j <= SIZE; j += i)
       if(num[j] % i == 0)  
         num[j] = 1;
-
+*/
   scanf("%d", &T);
 
   for(int i = 0; i < T; i++) {
     scanf("%d", &N);
+
+    for(int i = 2; i < sqrt(N); i++)
+      for(int j = i * i; j <= N; j += i)
+        if(num[j] % i == 0)
+          num[j] = 1;
 
     for(int j = N/2; j > 0; j--)
       if(num[j] == 0 && num[N-j] == 0) {

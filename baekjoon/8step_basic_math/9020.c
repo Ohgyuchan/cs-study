7b4c8808952b978d417c5a59b4f0a0e0aa46caa0
Ohgyuchan <gyu3637@gmail.com>
2021-06-20 20:39:34 +0900

    Solve 백준 9020

---
+++ baekjoon/8step_basic_math/9020.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#define SIZE 10001
+
+int num[SIZE] = { 0, };
+
+int main() {
+  int T, N;
+
+  for(int i = 2; i <= sqrt(SIZE); i++)
+    for(int j = i * i; j <= SIZE; j+= i)
+      if(num[j] % i == 0)  
+        num[j] = 1;
+
+  scanf("%d", &T);
+
+  for(int i = 0; i < T; i++) {
+    scanf("%d", &N);
+
+    for(int j = N/2; j > 0; j--)
+      if(num[j] == 0 && num[N-j] == 0) {
+        printf("%d %d\n", j, N-j);
+        break;
+      }
+  }
+
+  return 0;
+}

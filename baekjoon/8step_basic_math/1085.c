c0f7c7672992d65136ec95c2308ea20dc0af0906
Ohgyuchan <gyu3637@gmail.com>
2021-07-02 17:18:29 +0900

    Solve 백준 1085 직사각형에서 탈출

--- baekjoon/8step_basic_math/1085.c
+++ baekjoon/8step_basic_math/1085.c
@@ -2,7 +2,20 @@
 
 int main () {
   int x, y, w, h;
-
+  
   scanf("%d %d %d %d", &x, &y, &w, &h);
 
+  int min = x;
+
+  if(min > y)
+    min = y;
+  
+  if(min > w - x)
+    min = w - x;
+  
+  if(min > h - y)
+    min = h - y;
+
+  printf("%d\n", min);
+  return 0;
 }

863e48e1efac2eb06e6e27fa452ddcceece92f7e
Ohgyuchan <gyu3637@gmail.com>
2021-07-04 20:41:40 +0900

    판정 수정

--- python/pygame_basic/poktanpihagi.py
+++ python/pygame_basic/poktanpihagi.py
@@ -4,7 +4,7 @@ import random
 pygame.init() #파이 게임 초기화
 SCREEN_WIDTH = 480
 SCREEN_HEIGHT = 640
-pygame.display.set_caption('남친피하기') # Game title
+pygame.display.set_caption('똥피하기') # Game title
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #화면 크기 설정
 clock = pygame.time.Clock() 
 
@@ -21,19 +21,21 @@ background_image = pygame.image.load("/Users/terman/dev/cs-study/python/pygame_b
 bomb_image = pygame.image.load('/Users/terman/dev/cs-study/python/pygame_basic/ddong.png')
 bombs = []
 for i in range(10):
-    rect = pygame.Rect(bomb_image.get_rect())
-    rect.left = random.randint(0, SCREEN_WIDTH)
+    rect = bomb_image.get_rect()
+    rect.left = random.randint(0, SCREEN_WIDTH - rect.width)
     rect.top = -100
-    dy = random.randint(3, 9)
+    dy = random.randint(3, 8)
     bombs.append({'rect': rect, 'dy': dy})
 
 person_image = pygame.image.load('/Users/terman/dev/cs-study/python/pygame_basic/person.png')
-person = pygame.Rect(person_image.get_rect())
-person.left = SCREEN_WIDTH // 2 - person.width // 2
+person_rect = pygame.Rect(person_image.get_rect())
+person = person_rect.inflate(5, 5)
+person.left = SCREEN_WIDTH / 2 - person.width / 2
 person.top = SCREEN_HEIGHT - person.height
 person_dx = 0
 person_dy = 0
 running = True
+
 while running: #게임 루프
     screen.blit(background_image, (0, 0)) #단색으로 채워 화면 지우기
 
@@ -72,13 +74,13 @@ while running: #게임 루프
         person.left = SCREEN_WIDTH - person.width
 
     #화면 그리기
-
     for bomb in bombs:
         screen.blit(bomb_image, bomb['rect'])
         # 충돌 조건
-        if person.colliderect(rect):
-            print("충돌!")
-            running = False
+        if person.top + 13 <= bomb['rect'].bottom:
+            if person.left + 13 <= bomb['rect'].right and person.right >= bomb['rect'].left + 13:
+                print("충돌!")
+                running = False
 
     screen.blit(person_image, person)
     counter = game_font.render(str(int(count)), True, (0, 0, 0))
@@ -87,3 +89,6 @@ while running: #게임 루프
 
     pygame.display.update() #모든 화면 그리기 업데이트
     clock.tick(30) #30 FPS (초당 프레임 수) 를 위한 딜레이 추가, 딜레이 시간이 아닌 목표로 하는 FPS 값
+
+# pygame 종료
+pygame.quit()

cb5035aba285c2383221e772c09d5e7e549519f8
Ohgyuchan <gyu3637@gmail.com>
2021-07-11 17:47:14 +0900

    Solve 백준 2447 별찍기 -10

--- baekjoon/9step_recursive/2447.c
+++ baekjoon/9step_recursive/2447.c
@@ -1,51 +1,51 @@
 #include <stdio.h>
 #include <math.h>
  
-char arr[2500][2500];
+char star[2500][2500];
  
 //모든 칸을 *로 채워주기
-void AllStar(int n) {
+void arrange_star(int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++)
-            arr[i][j] = '*';
+            star[i][j] = '*';
     }
 }
  
-//지정된 점(x,y)부터 가로새로로 3^cnt만큼 공백으로 채우기
-void Blank(int x, int y, int cnt) {
-    for (int i = x; i < x + (int)pow(3, cnt); i++) {
-        for (int j = y; j < y + (int)pow(3, cnt); j++) {
-            arr[i][j] = ' ';
+//지정된 점(x,y)부터 가로새로로 3^count만큼 공백으로 채우기
+void insert_blank_to_star(int x, int y, int count) {
+    for (int i = x; i < x + (int)pow(3, count); i++) {
+        for (int j = y; j < y + (int)pow(3, count); j++) {
+            star[i][j] = ' ';
         }
     }
 }
  
 //공백으로 지정할 점(x,y) 찾기
-void Pivot(int n, int cnt) {
+void find_blank(int n, int count) {
     if (n > 0) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 //해당 기준점을 공백으로
-                Blank((int)pow(3, cnt) + i * (int)pow(3, cnt + 1), 
-                    (int)pow(3, cnt) + j * (int)pow(3, cnt + 1), cnt);
+                insert_blank_to_star((int)pow(3, count) + i * (int)pow(3, count + 1), 
+                    (int)pow(3, count) + j * (int)pow(3, count + 1), count);
             }
         }
         //기준점 이동하기
-        Pivot(n / 3, cnt + 1);
+        find_blank(n / 3, count + 1);
     }
 }
  
 int main() {
     int N;
     scanf("%d", &N);
-    AllStar(N);
+    arrange_star(N);
  
-    Pivot(N / 3, 0);
+    find_blank(N / 3, 0);
  
     //결과출력
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++)
-            printf("%c", arr[i][j]);
+            printf("%c", star[i][j]);
         printf("\n");
     }
     return 0;

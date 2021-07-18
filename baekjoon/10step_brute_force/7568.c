dcb2c03f1d7e35b6632919311056bff2a1516221
Ohgyuchan <gyu3637@gmail.com>
2021-07-18 22:16:44 +0900

    Solve 백준 7568 덩치

--- baekjoon/10step_brute_force/7568.c
+++ baekjoon/10step_brute_force/7568.c
@@ -2,10 +2,25 @@
 
 int main() {
   int N;
+  int count;
+  int arr[50][2]
   scanf("%d", &N);
 
   for(int i = 0 ; i < N ; i++) {
-    scanf("")
+    scanf("%d %d", &arr[i][0], &arr[i][1]);
   }
+
+  for(int i = 0; i < N; i++) {
+    count = 0;
+
+    for(int j = 0; j < N; j++) {
+      if(arr[i][0] < arr[j][0])
+        count++;
+    }
+    
+    printf("%d", count + 1);
+
+  }
+
   return 0;
 }

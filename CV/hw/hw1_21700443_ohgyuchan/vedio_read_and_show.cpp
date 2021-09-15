e99688b3e478f78d0c42e34e44df0d96d019c949
Ohgyuchan <gyu3637@gmail.com>
2021-09-16 05:39:48 +0900

    update cv_hw1

--- CV/hw/hw1_21700443_ohgyuchan/vedio_read_and_show.cpp
+++ CV/hw/hw1_21700443_ohgyuchan/vedio_read_and_show.cpp
@@ -1,5 +1,6 @@
 #include "opencv/cv.hpp"
 #include <iostream>
+#include <time.h>
 
 using namespace cv;
 using namespace std;
@@ -8,11 +9,11 @@ int main() {
     Mat frame;
     VideoCapture cap;
     
-    // check if file exists. if none program ends
     if (cap.open("background.mp4") == 0) {
         cout << "no such file!" << endl;    
         waitKey(0);
     }
+
     while (1) {
         cap >> frame;
         if (frame.empty()) {
@@ -22,11 +23,19 @@ int main() {
         int msec = cap.get(CAP_PROP_POS_MSEC);
         int frames = cap.get(CAP_PROP_POS_FRAMES);
         int total_frames = cap.get(CAP_PROP_FRAME_COUNT);
+        int fps = cap.get(CAP_PROP_FPS);
+
         if(msec >= 3000) {
             break;
         }
+
         cout << "frames: " << frames <<"/" << total_frames << endl;
+
         imshow("video", frame);
-        waitKey(1370/72);
+
+        //fps: 24, 1000/24: 41.6666666667
+        waitKey(1000/fps);
     }
+    
+    return 0;
 }

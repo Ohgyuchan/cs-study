b25563b534918ff8ee17ecc7bea6334e284a7279
Ohgyuchan <gyu3637@gmail.com>
2021-09-29 16:17:48 +0900

    Update hw3 code

--- CV/hw/hw3/main.cpp
+++ CV/hw/hw3/main.cpp
@@ -12,32 +12,31 @@ int main() {
     moon = imread("moon.png");
     saltnpepper = imread("saltnpepper.png");
 
-    Rect lena_rect(0, 0, lena.cols/2, lena.rows);
-    Mat lena_rect_roi = lena(lena_rect);
+    lena_filtered = lena.clone();
+    Rect lena_rect(0, 0, lena_filtered.cols/2, lena_filtered.rows);
+    Mat lena_rect_roi = lena_filtered(lena_rect);
     
-	blur(lena_rect_roi, lena_filtered, Size(7, 7));
-
-    Rect moon_rect(moon.cols/2, 0, moon.cols/2, moon.rows);
-    Mat moon_rect_roi = moon(moon_rect);
+	blur(lena_rect_roi, lena_rect_roi, Size(7, 7));
     
-    Rect moon_rect2(0, 0, moon.cols/2, moon.rows);
-    Mat moon_rect_roi2 = moon(moon_rect2);
+    moon_filtered = moon.clone();
+    Rect moon_rect(moon_filtered.cols/2, 0, moon_filtered.cols/2, moon_filtered.rows);
+    Mat moon_rect_roi = moon_filtered(moon_rect);
+    Mat moon_rect_roi_laplacian, moon_rect_roi_abs;
 	
-    Laplacian(moon_rect_roi, moon_filtered, CV_16S);
-	convertScaleAbs(moon_filtered, moon_filtered);
-	moon_filtered = moon_filtered + moon_rect_roi;
+    Laplacian(moon_rect_roi, moon_rect_roi_laplacian, CV_16S);
+	convertScaleAbs(moon_rect_roi_laplacian, moon_rect_roi_abs);
+	moon_rect_roi = moon_rect_roi + moon_rect_roi_abs;
 
     medianBlur(saltnpepper, saltnpepper_filtered, 9);
 
-	// imshow("lena", lena);
-    // imshow("lena_filtered", lena_filtered);
+	imshow("lena", lena);
+    imshow("lena_filtered", lena_filtered);
 
     imshow("moon", moon);
 	imshow("moon_filtered", moon_filtered);
-	imshow("moon_filtered2", moon_rect_roi2);
 
-	// imshow("saltnpepper", saltnpepper);
-	// imshow("saltnpepper_filtered", saltnpepper_filtered);
+	imshow("saltnpepper", saltnpepper);
+	imshow("saltnpepper_filtered", saltnpepper_filtered);
 	
 	waitKey(0);
 

6c2f4a2d1f0bed3b136b95b05912cc6c6a5bd1dc
Ohgyuchan <gyu3637@gmail.com>
2021-10-10 23:34:57 +0900

    Update 2751.c

--- baekjoon/11step_sort/2750.c
+++ baekjoon/11step_sort/2750.c
@@ -2,18 +2,14 @@
 
 int a[1000] = {0, };
 
-void ascending_sort(int n) {
-  for(int i = 0; i < n; i++) {
-    for(int j = 0; j < n; j++) {
-      int temp;
-      if(a[i] < a[j]) {
-        temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-      }
-    }
-  }
-}
+
+// void selection_sort;
+// void insertion_sort;
+// void bubble_sort;
+// void heap_sort;
+// void merge_sort;
+// void quick_sort;
+
 
 int main() {
   int N;
@@ -23,8 +19,6 @@ int main() {
   for(int i = 0; i < N; i++) {
     scanf("%d", &a[i]);
   }
-
-  ascending_sort(N);
 
   for(int i = 0; i < N; i++) {
     printf("%d\n", a[i]);

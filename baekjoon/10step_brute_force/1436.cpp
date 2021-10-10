cf0199514dd822d4e32a48dbd845fb5d2d2812a5
Ohgyuchan <gyu3637@gmail.com>
2021-10-10 14:31:04 +0900

    Update 1436.cpp

--- baekjoon/10step_brute_force/1436.cpp
+++ baekjoon/10step_brute_force/1436.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    int N;
-    cin >> N;
+int main(int argc, char *argv[])
+{
+	int n;
+	int count = 0;
+	int title = 666;
+	string s;
 
-    cout << N << 666 << endl;
+	cin >> n;
 
-    return 0;
+	while(true) {
+		s = to_string(title);
+        cout << s << endl;
+
+		if (s.find("666") != -1) { // if s have "666"
+			++count;
+		}
+
+		if (count == n) {
+			cout << title << endl;
+			break;
+		}
+
+        title++;
+	}
+
+	return 0;
 }

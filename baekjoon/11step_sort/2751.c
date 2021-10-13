3e7cf6b775c26a2b0aa8a505b7a5d90de3966050
Ohgyuchan <gyu3637@gmail.com>
2021-10-13 18:53:33 +0900

    Update 2751.c

--- baekjoon/11step_sort/2751.c
+++ baekjoon/11step_sort/2751.c
@@ -1,12 +1,44 @@
 // void merge_sort;
 #include <stdio.h>
-#define MAX 1000001
 
-int num[MAX] = {0, };
+int num[1000000] = {0, };
+int N;
+
+void merge (int data[], int p, int q, int r) {
+    int i = p, j = q+1, k = p;
+    int tmp[r]; // 새 배열
+    while(i<=q && j<=r) {
+        if(data[i] <= data[j]) tmp[k++] = data[i++];
+        else tmp[k++] = data[j++];
+    }
+    while(i<=q) tmp[k++] = data[i++];
+    while(j<=r) tmp[k++] = data[j++];
+    for(int a = p; a<=r; a++) data[a] = tmp[a];
+}
+
+void merge_sort(int data[], int p, int r) {
+    int q;
+    if(p < r) {
+        q = (p+r)/2;
+        merge_sort(data, p , q);
+        merge_sort(data, q+1, r);
+        merge(data, p, q,  r);
+    }
+}
+void print_out() {
+    for(int i = 0; i < N; i++)
+        printf("%d\n", num[i]);
+}
 
 int main() {
-    int N;
     scanf("%d", &N);
+
+    for(int i = 0; i < N; i++)
+        scanf("%d", &num[i]);
+
+    merge_sort(num, 0, N-1);
+
+    print_out();
 
     return 0;
 }

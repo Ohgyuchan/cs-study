8ed9b0639475e798698b003319d5926982a1cdcd
Ohgyuchan <gyu3637@gmail.com>
2021-10-16 00:45:40 +0900

    Update hw5 code

--- CV/hw/hw5/main.cpp
+++ CV/hw/hw5/main.cpp
@@ -7,14 +7,22 @@ using namespace cv;
 int main() {
     Mat lena, colorful, balancing;
 
+    Mat lena_output, colorful_output, balancing_output;
+
     lena = imread("lena.png");
     colorful = imread("colorful.jpg");
     balancing = imread("balancing.jpg");
 
+    lena_output = lena;
+    colorful_output = colorful;
+    balancing_output = balancing;
+
+
+
     while(1) {
-        imshow("lena", lena);
-        imshow("colorful", colorful);
-        imshow("balancing", balancing);
+        imshow("lena", lena_output);
+        imshow("colorful", colorful_output);
+        imshow("balancing", balancing_output);
 
         if(waitKey() == 27) {
             break;

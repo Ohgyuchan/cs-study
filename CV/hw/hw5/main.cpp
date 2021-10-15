fb458c9a73026e10d543aa1395e006f26e73095e
Ohgyuchan <gyu3637@gmail.com>
2021-10-16 00:40:33 +0900

    Update hw5 code

--- CV/hw/hw5/main.cpp
+++ CV/hw/hw5/main.cpp
@@ -5,5 +5,21 @@ using namespace std;
 using namespace cv;
 
 int main() {
+    Mat lena, colorful, balancing;
+
+    lena = imread("lena.png");
+    colorful = imread("colorful.jpg");
+    balancing = imread("balancing.jpg");
+
+    while(1) {
+        imshow("lena", lena);
+        imshow("colorful", colorful);
+        imshow("balancing", balancing);
+
+        if(waitKey() == 27) {
+            break;
+        }
+    }
+
     return 0;
 }

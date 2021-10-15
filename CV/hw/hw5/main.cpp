8e08c7a7131b840b7bd82cf6ff1e9c633e844aee
Ohgyuchan <gyu3637@gmail.com>
2021-10-16 01:31:30 +0900

    Update negative transformation code

--- CV/hw/hw5/main.cpp
+++ CV/hw/hw5/main.cpp
@@ -4,16 +4,29 @@
 using namespace std;
 using namespace cv;
 
+class Lena {
+    public:
+        void negative(Mat scr, Mat &dst) {
+            for (int row = 0; row < scr.rows; row++) { 
+                for (int col = 0; col < scr.cols; col++) { 
+                    for(int i = 0; i < 3; i++)
+                        dst.at<Vec3b>(row, col)[i] = 255 - scr.at<Vec3b>(row, col)[i]; // access RGB
+                }
+            }
+        }
+};
+
 int main() {
     Mat lena, colorful, balancing;
 
     Mat lena_output, colorful_output, balancing_output;
 
+    Lena L1;
+
     lena = imread("lena.png");
     colorful = imread("colorful.jpg");
     balancing = imread("balancing.jpg");
 
-    lena_output = lena;
     colorful_output = colorful;
     balancing_output = balancing;
 
@@ -23,17 +36,15 @@ int main() {
         // imshow("balancing", balancing_output);
         int key = waitKey();
 
-        if(key == 78 || key == 110) {
-            for (int row = 0; row < lena.rows; row++) { 
-                for (int col = 0; col < lena.cols; col++) { 
-                    for(int i = 0; i < 3; i++)
-                        lena_output.at<Vec3b>(row, col)[i] = 255 - lena.at<Vec3b>(row, col)[i]; 
-                }
-            }
-            // imshow("lena", lena_output);
+        if(key == 71 || key == 103) {
+            cout << key << endl;
         }
 
-        if(key == 27) {
+        else if(key == 78 || key == 110) {
+            L1.negative(lena, lena_output);
+        }
+
+        else if(key == 27) {
             break;
         }
     }

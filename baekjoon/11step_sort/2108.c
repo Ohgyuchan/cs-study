b4d6fa5f1cb93cf81a436ec73cdb171bb7038ac1
Ohgyuchan <gyu3637@gmail.com>
2021-10-18 16:20:11 +0900

    Update 2108.c

--- baekjoon/11step_sort/2108.c
+++ baekjoon/11step_sort/2108.c
@@ -7,8 +7,6 @@
 #include <stdio.h>
 
 int num[500000] = {0, };
-int minus[4001] = {0, };
-int plus[4001] = {0, };
 
 void merge (int data[], int p, int q, int r) {
     int i = p, j = q+1, k = p;
@@ -57,12 +55,50 @@ void descending_merge_sort(int data[], int p, int r) {
     }
 }
 
+int mode(int list[], int n)
+{
+	int ar[8001] = { 0 };
+	int i, idx, max = 0, cnt = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		idx = list[i] + 4000;
+		ar[idx] += 1;
+		
+		if (ar[idx] > max)
+			max = ar[idx];
+	}
+
+	for (i = 0, idx = 0; i < 8001 ; i++)
+	{
+		if (ar[i] == 0)
+			continue;
+
+		if (ar[i] == max)
+		{
+			if (cnt == 0)
+			{
+				idx = i;
+				cnt += 1;
+			}
+			else if (cnt == 1)
+			{
+				idx = i;
+				break;
+			}
+		}
+	}
+	return idx - 4000;
+
+}
+
 int main() {
     int n;
     float avg = 0;
     int mid = 0;
     int total = 0;
     int freq = 0;
+    int sec_freq = 0;
     int range = 0;
     
     scanf("%d", &n);
@@ -70,26 +106,123 @@ int main() {
     for(int i = 1; i <= n; i++) {
         scanf("%d", &num[i]);
         total += num[i];
-        if(num[i] < 0) {
-            minus[num[i]]++;
-        } else {
-            plus[num[i]]++;
-        }
     }
 
     merge_sort(num, 1, n);
-    descending_merge_sort(minus, 0, num[n]);
-    descending_merge_sort(plus, 0, num[n]);
 
-    avg = total / n;
+    avg = (float)total / n;
     mid = num[(n / 2) + 1];
-    freq = minus[n-1] >= plus[n-1] ? minus[n-1] : plus[n-1];
     range = num[n] - num[1];
 
     printf("%.0f\n", avg);
     printf("%d\n", mid);
-    printf("%d\n", freq);
+    printf("%d\n", mode(num, n+1));
     printf("%d\n", range);
 
     return 0;
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <math.h>
+
+// int compare(const void* first, const void* second)
+// {
+// 	int* a = (int*)first;
+// 	int* b = (int*)second;
+
+// 	if (*a < *b)
+// 		return -1;
+// 	else if (*a > *b)
+// 		return 1;
+// 	else
+// 		return 0;
+// }
+
+// int arith(int list[], int n)
+// {
+// 	double sum = 0;
+
+// 	for (int i = 0; i < n; i++)
+// 	{
+// 		sum += (list[i]);
+// 	}
+
+// 	return round(sum / n);
+// }
+
+// int median(int list[], int n)
+// {
+// 	if (n == 1)
+// 		return list[0];
+// 	else
+// 		return list[(n + 1) / 2 - 1];
+// }
+
+// int Mode(int list[], int n)
+// {
+// 	int ar[8001] = { 0 };
+// 	int i, idx, max = 0, cnt = 0;
+
+// 	for (i = 0; i < n; i++)
+// 	{
+// 		idx = list[i] + 4000;
+// 		ar[idx] += 1;
+		
+// 		if (ar[idx] > max)
+// 			max = ar[idx];
+// 	}
+
+// 	for (i = 0, idx = 0; i < 8001 ; i++)
+// 	{
+// 		if (ar[i] == 0)
+// 			continue;
+
+// 		if (ar[i] == max)
+// 		{
+// 			if (cnt == 0)
+// 			{
+// 				idx = i;
+// 				cnt += 1;
+// 			}
+// 			else if (cnt == 1)
+// 			{
+// 				idx = i;
+// 				break;
+// 			}
+// 		}
+// 	}
+// 	return idx - 4000;
+
+// }
+
+// int range(int list[], int n)
+// {
+// 	int max = list[n - 1];
+// 	int min = list[0];
+
+// 	return max - min;
+// }
+
+// int main()
+// {
+// 	int i, n;
+// 	int* list;
+
+// 	scanf("%d", &n);
+// 	list = (int*)calloc(n, sizeof(int));
+
+// 	for (i = 0; i < n; i++)
+// 	{
+// 		scanf(" %d", &list[i]);
+// 	}
+	
+// 	qsort(list, n, sizeof(list[0]), compare);
+
+// 	printf("%d\n", arith(list, n));
+// 	printf("%d\n", median(list, n));
+// 	printf("%d\n", Mode(list, n));
+// 	printf("%d\n", range(list, n));
+	
+// 	return 0;
+// }

aa53864ebc940121e84223b8663335171e87bb60
Ohgyuchan <gyu3637@gmail.com>
2021-10-20 18:12:59 +0900

    Update edge detection code

--- CV/hw/hw6/main.cpp
+++ CV/hw/hw6/main.cpp
@@ -3,18 +3,24 @@
 using namespace cv;
 
 int main() {
-    Mat lena, sobel, canny;
+    Mat lena, sobel, grad_x, grad_y, canny;
     // the different results with color, gray scale.
-    lena = imread("lena.png");
+    lena = imread("lena.png", 0);
 
     imshow("Lena", lena);
-    int ddepth = CV_8U;
+
+    GaussianBlur(lena, sobel, Size(5,5), BORDER_DEFAULT);
+    
+    Sobel(sobel, grad_x, CV_16S, 1, 0, 3);
+    convertScaleAbs(grad_x, grad_x);
+
+    Sobel(sobel, grad_y, CV_16S, 0, 1, 3);
+    convertScaleAbs(grad_y, grad_y);
+    
+    addWeighted(grad_x, 0.5, grad_y, 0.5, 0, sobel);
 
     Canny(lena, canny, 190, 200, 3);
     imshow("Canny", canny);
-    lena.copyTo(sobel);
-    Sobel(lena, sobel, ddepth, 1, 1);
-    threshold(sobel, sobel, 25, 255, THRESH_BINARY);
 
     imshow("Sobel",sobel);
 

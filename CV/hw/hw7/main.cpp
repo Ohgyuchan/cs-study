1de8baa824917427d6394876bd05c7f78855edff
Ohgyuchan <gyu3637@gmail.com>
2021-11-06 19:47:01 +0900

    Update hw7 code

--- CV/hw/hw7/main.cpp
+++ CV/hw/hw7/main.cpp
@@ -1,41 +1,8 @@
+#include <iostream>
 #include "opencv/cv.hpp"
 
 using namespace cv;
 using namespace std;
-
-Mat drawHistogram(Mat src){
-    Mat hist, histImage;
-    // establish the number of bins
-    int i, hist_w, hist_h, bin_w, histSize;
-    float range[] = { 0, 256 };
-    const float* histRange = { range };
-    int channels[] = {0};
-
-    // Set the number of bins to 16
-    histSize = 16;
-    hist_w = 512;
-    hist_h = 512;
-    bin_w = cvRound((double)hist_w / histSize);
-    
-    //draw the histogram
-    histImage = Mat(hist_h, hist_w, CV_8UC3, Scalar(255, 255, 255));
-    
-    // compute the histograms
-    // &src: input image, 1: #of src image, 0: #of channels numerated from 0 ~ channels()-1, Mat(): optional mask
-    // hist: output histogram, 1: histogram dimension, &histSize: array of histogram size, &histRange: array of histogram’s boundaries
-    calcHist(&src, 1, channels, Mat(), hist, 1, &histSize, &histRange);
-    
-    // Fit the histogram to [0, histImage.rows]
-    // hist: input Mat, hist: output Mat, 0: lower range boundary of range normalization, histImage.rows: upper range boundary
-    // NORM_MINMAX: normalization type, -1: when negative, the ouput array has the same type as src, Mat(): optional mask
-    normalize(hist, hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-
-    for (i = 0; i < histSize; i++) {
-        rectangle(histImage, Point(bin_w * i, hist_h), Point(bin_w * i+hist_w/histSize, hist_h - cvRound(hist.at<float>(i))), Scalar(0, 0, 0), -1);
-    }
-
-    return histImage;
-}
 
 int main() {
     Mat finger_print, adaptive_1, adaptive;
@@ -44,25 +11,30 @@ int main() {
     adaptive_1 = imread("adaptive_1.jpg", 0);
     adaptive = imread("adaptive.png", 0);
 
-    Mat finger_hist, adaptive_1_hist, adaptive_hist;
-    finger_hist = drawHistogram(finger_print);
-    adaptive_1_hist = drawHistogram(adaptive_1);
-    adaptive_hist = drawHistogram(adaptive);
+    Mat dst;
 
-    imshow("finger_hist", finger_hist);
-    imshow("adaptive_1_hist", adaptive_1_hist);
-    imshow("adaptive_hist", adaptive_hist);
+    for (int j = 0; j < adaptive.rows; j++) {
+        for (int i = 0; i < adaptive.cols; i++) {
+            if(adaptive.at<uchar>(j, i) == 100) {
+                dst.at<uchar>(j, i) = adaptive.at<uchar>(j, i);
+            } else {
+                dst.at<uchar>(j, i) = adaptive.at<uchar>(j, i) + 100;
+            }
+        }
+    }
 
+    
     // OTSU Algorithm
-    threshold(finger_print, finger_print, 0, 255, THRESH_BINARY | THRESH_OTSU);
+    // threshold(finger_print, finger_print, 0, 255, THRESH_BINARY | THRESH_OTSU);
 
-    adaptiveThreshold(adaptive_1, adaptive_1, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 7, 10);
+    // adaptiveThreshold(adaptive_1, adaptive_1, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 55, 10);
 
-    adaptiveThreshold(adaptive, adaptive, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 11, 0);
+    // adaptiveThreshold(adaptive, adaptive, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 55, 10);
 
-    imshow("finger_print", finger_print);
-    imshow("adaptive_1", adaptive_1);
+    // imshow("finger_print", finger_print);
+    // imshow("adaptive_1", adaptive_1);
     imshow("adaptive", adaptive);
+    imshow("dst", dst);
 
     waitKey(0);
 

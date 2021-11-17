c44b2e476416f517a6b841d51619baeffafbefe7
Ohgyuchan <gyu3637@gmail.com>
2021-11-17 18:04:01 +0900

    Update hw8 code

--- CV/hw/hw8/main.cpp
+++ CV/hw/hw8/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
     VideoCapture cap("background.mp4");
     int count = 2;
-    Mat background, image, gray, result;
+    Mat background, frame, current_frame_as_gray, result, final_result;
     
     cap >> background;
     cvtColor(background, background, COLOR_BGR2GRAY);
@@ -17,11 +17,11 @@ int main() {
         int frames =  cap.get(CAP_PROP_POS_FRAMES);
         int fps = cap.get(CAP_PROP_FPS);
         
-        if(!cap.read(image)) break;
+        if(!cap.read(frame)) break;
 
-        cvtColor(image, image, COLOR_BGR2GRAY);
+        cvtColor(frame, frame, COLOR_BGR2GRAY);
         
-        add(image / count, background*(count - 1) / count, background);
+        add(frame / count, background*(count - 1) / count, background);
 
         if(frames == 10) break;
 
@@ -34,18 +34,35 @@ int main() {
         int fps = cap.get(CAP_PROP_FPS);
         if(cap.grab() == 0) break;
 
-        cap.retrieve(image);
-        cvtColor(image, gray, CV_BGR2GRAY);
+        cap.retrieve(frame);
+        cvtColor(frame, current_frame_as_gray, CV_BGR2GRAY);
 
-        absdiff(background, gray, result);
-        threshold(result, result, 20, 255, CV_THRESH_BINARY);
+        absdiff(current_frame_as_gray, background, result);
+        final_result = result.clone();
+        medianBlur(final_result, final_result, 11);
+        threshold(final_result, final_result, 20, 255, CV_THRESH_BINARY);
         
-        vector<vector<Point>> contours;
+        vector<vector<Point> > contours;
         vector<Vec4i> hierarchy;
-        findContours(result, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+        findContours(final_result, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+        
+        int count_400px = 0;
+        vector<Rect> boundRect(contours.size());
+        for (int i = 0; i < contours.size(); i++)
+            if(contourArea(Mat(contours[i])) >= 400) {
+                boundRect[i] = boundingRect(Mat(contours[i]));
+                count_400px++;
+            }
+            
+        //draw rectangles on the contours
+        for (int i = 0; i < contours.size(); i++) {
+            rectangle(final_result, boundRect[i].tl(), boundRect[i].br(), Scalar(255, 255, 255), 2, 8, 0);
+            putText(final_result, format("contour count: %d", count_400px), Point(50, 80), FONT_HERSHEY_SIMPLEX, 1, Scalar(128), 4);
+        }
 
+        imshow("Background", background);
         imshow("Result(x, y)", result);
-
+        imshow("final_result", final_result);
 
         waitKey(1000/fps);
     }

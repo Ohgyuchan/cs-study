09f827aa89620d88e3ffb12ddf84df0222e41f2a
Ohgyuchan <gyu3637@gmail.com>
2021-11-17 14:24:37 +0900

    Update hw8 code

--- CV/hw/hw8/main.cpp
+++ CV/hw/hw8/main.cpp
@@ -10,24 +10,35 @@ int main() {
     Mat background, image, gray, result, foregroundMask,foregroundImg;
     
     cap >> background;
+    cvtColor(background, background, COLOR_BGR2GRAY);
 
+    // set background as the average of the first 10 frames.
     while(1) {
+        int frames =  cap.get(CAP_PROP_POS_FRAMES);
+        int fps = cap.get(CAP_PROP_FPS);
+        cout << "frames: " << frames << endl;
+        
         if(!cap.read(image)) break;
+
+        cvtColor(image, image, COLOR_BGR2GRAY);
+        
         add(image / count, background*(count - 1) / count, background);
 
-        imshow("avg", background);
-        if(count == 10) break;
-        count++;
-        waitKey(33);
+        if(frames == 10) break;
+
+        waitKey(1000/fps);
     }
-    
+
+    cap.set(CAP_PROP_POS_FRAMES, 0);
+
     while(1) {
         if(cap.grab() == 0) break;
+        waitKey(0);
         cap.retrieve(image);
         cvtColor(image, gray, CV_BGR2GRAY);
 
         absdiff(background, gray, foregroundMask);
-        threshold(foregroundMask, foregroundMask, 50, 255, CV_THRESH_BINARY);
+        threshold(foregroundMask, foregroundMask, 20, 255, CV_THRESH_BINARY);
         foregroundMask.copyTo(foregroundImg);
         gray.copyTo(foregroundImg, foregroundMask);
 

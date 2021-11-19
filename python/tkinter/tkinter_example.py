489552a37eaf1689f51c93cb6fa6e56c46f238d6
Ohgyuchan <gyu3637@gmail.com>
2021-11-19 22:07:48 +0900

    Update tkinter_example.py

--- python/tkinter/tkinter_example.py
+++ python/tkinter/tkinter_example.py
@@ -1,13 +1,36 @@
 from tkinter import *
+from tkinter.colorchooser import *
+
+def callback():
+    global result
+    result = askcolor(title="Color Chooser")
+    result = result[1]
+
 root = Tk()
- 
-lbl = Label(root, text="이름")
-lbl.pack()
- 
-txt = Entry(root)
-txt.pack()
- 
-btn = Button(root, text="OK")
-btn.pack()
- 
+
+canvas = Canvas(root, width=500, height=500)
+color = 'red'
+result = '#476042'
+
+B1 = Button(root, text='Choose Color', fg="darkgreen", command=callback)
+B1.pack(side=LEFT, padx=10)
+
+lastx, lasty = 0, 0
+
+def xy(event):
+    global lastx, lasty
+    lastx, lasty = event.x, event.y
+
+def addLine(event):
+    global lastx, lasty
+    canvas.create_line((lastx, lasty, event.x, event.y), fill=result)
+    lastx, lasty = event.x, event.y
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+canvas.pack()
+canvas.bind("<Button-1>", xy)
+canvas.bind("<B1-Motion>", addLine)
+
 root.mainloop()

df4ec584fa8cc0e6e376b238ace4593f146937d5
Ohgyuchan <gyu3637@gmail.com>
2021-11-24 21:39:11 +0900

    Update hw9 code

--- CV/hw/hw9/main.cpp
+++ CV/hw/hw9/main.cpp
@@ -16,11 +16,13 @@ int main() {
 
     int count = 2;
     cap >> background;
+    cvtColor(background, background, COLOR_BGR2GRAY);
 
     // set background as the average of the first 10 frames.
     while(1) {
         if(!cap.read(frame)) break;
-        
+        cvtColor(frame, frame, CV_BGR2GRAY);
+
         add(frame / count, background*(count - 1) / count, background);
 
         if(cap.get(CAP_PROP_POS_FRAMES) == cap.get(CAP_PROP_FRAME_COUNT)) break;
@@ -31,6 +33,9 @@ int main() {
     int key = -1;
     int command = 0;
     cap.set(CAP_PROP_POS_FRAMES, 0);
+    
+    int blockSize = 3;
+    int c = 1;
 
     while (1) {
         if(!cap.read(frame)) {
@@ -39,13 +44,13 @@ int main() {
         }
         
         cvtColor(frame, gray, CV_BGR2GRAY);
-        absdiff(background, gray, foregroundMask);
-        threshold(foregroundMask, foregroundMask, 30, 255, CV_THRESH_BINARY);
+        absdiff(gray, background, foregroundMask);
+        threshold(foregroundMask, foregroundMask, 10, 255, THRESH_BINARY);
         erode(foregroundMask, foregroundMask, element);
-        adaptiveThreshold(foregroundMask, foregroundMask, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 51, 15);
+        adaptiveThreshold(foregroundMask, foregroundMask, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, blockSize, 11);
         
         if(key == -1) {
-            imshow("Face", background);
+            imshow("Face", foregroundMask);
         }
         if(key != -1) {
             if(command == key)
@@ -55,6 +60,13 @@ int main() {
         }
         if(command == 98) {
             imshow("Face", foregroundMask);
+        }
+
+        if(command == 'c') {
+            blockSize += 2;
+        }
+        if(command == 'C') {
+            blockSize -= 2;
         }
         key = waitKey(33);
     }

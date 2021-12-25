904f44812a1eb11cc45357150ce52d93287b1391
Ohgyuchan <gyu3637@gmail.com>
2021-12-25 20:17:13 +0900

    redirect racket directory

--- racket/midterm_practice.rkt
+++
@@ -1,9 +0,0 @@
-#lang plai
-(define-type AE
-  [num (n number?)]
-  [add (lhs num?)
-       (rhs num?)]
-  [sub (lhs num?)
-       (rhs num?)]);
-
-(add (num 1) (num 2));
--- racket/sum.rkt
+++
@@ -1,4 +0,0 @@
-#lang plai
-
-(define (jc b)(+ 1 b))
-(jc 3);

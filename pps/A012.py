569bad1b6ca501a967e577e594f78be54836bb7b
Ohgyuchan <gyu3637@gmail.com>
2022-07-06 00:12:40 +0900

    Solve A012.py

--- pps/A012.py
+++ pps/A012.py
@@ -1,17 +1,12 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
-        count = 0
-        if n <= 2 :
+        if n <=2 :
             return 0
+        prime = [1] * (n)
+        prime[0] = 0
+        prime[1] = 0
         
-        for i in range(2, n) :
-            primeCheck = True
-            for j in range(2, i + 1) :
-                if i != j and i % j == 0 :
-                    primeCheck = False
-                    break
-            
-            if primeCheck :
-                count += 1
-        
-        return count
+        for i in range(2,n):
+            if prime[i]:
+                prime[i+i:n:i] = [0] * len(prime[i+i:n:i]) # indexing, prime + prime = non-prime
+        return sum(prime)

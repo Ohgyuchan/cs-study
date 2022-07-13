09c25233c20bbe2411a3c5ae5c8c78bbf446abf6
Ohgyuchan <gyu3637@gmail.com>
2022-07-13 13:24:47 +0900

    DELETE PPS

--- pps/A015.py
+++
@@ -1,7 +0,0 @@
-nums = list(map(int, input().split()))
-
-answer = 0
-for num in nums :
-    answer += num ** 2
-answer %= 10
-print(answer)
--- pps/A021.py
+++
@@ -1,9 +0,0 @@
-import sys
-input = sys.stdin.readline
-
-n = int(input())
-answer = 0
-for i in range(n) :
-    answer += int(input()) - 1
-    
-print(answer + 1)
